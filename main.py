b758c0f628e9e03674be1c59bb78aac4f36ce25b
Franco Aller <faller@uade.edu.ar>
2025-02-19 17:28:31 -0300

    solo falta el ultimo punto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from tabulate import tabulate
 
 
 def menu()-> None:
@@ -10,10 +11,11 @@ def menu()-> None:
                 print("archivo cargado con exito!!")
                 print(f"La cantidad de universisdades es: {cantidad}")
         elif op == "b":
-            nombre = input("Ingrese nombre o parte del nombre: ")
-            buscar_universidad(nombre)
+            palabra = input("Ingrese nombre o parte del nombre: ")
+            buscar_universidad(palabra)
         elif op == "c":
-            exportar_alumnos()
+            nombre = input("Ingrese el nombre de la facultad: ")
+            exportar_alumnos(nombre)
         elif op == "d":
             mujeres_egresadas()
         elif op == "z":
@@ -40,50 +42,49 @@ def importar_datos(archivo_csv):
         with open(archivo_csv, "r", encoding= "utf-8") as entrada:
             linea_uno = entrada.readline()
             
-            encabezado = linea_uno.strip().split(",")
+            encabezado = [celda.strip() for celda in linea_uno.strip().split(",")]
+
+            print(len(encabezado))
 
             while True:
                 linea = entrada.readline()
                 if not linea:
                     break
-                columnas = linea.strip().split(",")
+                columnas = [celda.strip() for celda in linea.strip().split(",")]
+                
 
-                if columnas[2].startswith("Universidad"):
+                if columnas and columnas[2].startswith("Universidad"):
                     universidades.append(columnas)
                     contador += 1
             
             for indice, columna in enumerate(encabezado):
                 lista = []
                 for fila in universidades:
-                    lista.append(fila[indice])
+                    if not fila[indice]:
+                        lista.append("0")
+                    else:        
+                        lista.append(fila[indice])
 
                 datos_uni[columna] = lista
 
     except FileNotFoundError:
         print("archivo no encontrado..")
+    except IndexError as e:
+        print(f"Error: {e}")
 
-    
-    
     try:        
-        with open("./universidades.JSON", "w", encoding="utf-8") as uni_json:
-            json.dumps(datos_uni)
+        with open("./universidades.JSON", "w", encoding="utf-8") as f:
+            json.dump(datos_uni, f, ensure_ascii=False, indent=4)
     
-    except FileNotFoundError:    
+    except FileNotFoundError:
         print("El archivo no existe...") 
-
-        """with open("./unviersidades.csv", "w", encoding="utf-8") as salida:
-            salida.write(encabezado)
-            for filas in universidades:
-                fila = ",".join(filas)
-                salida.write(fila + "\n")
-        """
 
     return contador
     
  
 
 
-def buscar_universidad(nombre):
+def buscar_universidad(palabra):
     """
     Busca por nombre todas las universidades que coincidan con el nombre ingresado
 
@@ -91,14 +92,65 @@ def buscar_universidad(nombre):
 
     post: devuelve None
     """
-    pass
 
-def exportar_alumnos():
-    pass
+    datos = {
+        "Año": [],
+        "Institución": [],
+        "Título": [],
+        "Egresados Mujeres": []
+    }
+
+    try:
+        with open("universidades.JSON", "r", encoding="utf-8") as f:
+            uni = json.load(f)
+            print(len(uni["Año"]))
+            print(len(uni["Institución"]))
+            print(len(uni["Título"]))
+            print(len(uni["Egresados Mujeres"]))
+            for indice, universidad in enumerate(uni["Institución"]):
+                if palabra.lower() in universidad.lower():
+                    datos["Año"].append(uni["Año"][indice])
+                    datos["Institución"].append(uni["Institución"][indice])
+                    datos["Título"].append(uni["Título"][indice])
+                    datos["Egresados Mujeres"].append(uni["Egresados Mujeres"][indice])
+            if datos:
+                print(tabulate(datos, headers="keys", tablefmt="pretty"))
+            else:
+                print("Aún no hay datos cargados.")
+
+    except FileNotFoundError:
+        print("El archivo no existe...")
+    except IndexError as e:
+        print(f"Error: {e}")
+        
+    return None
+
+def exportar_alumnos(nombre):
+    """
+    exportar una los datos de una unviersidad de la cual el usuario ingresa el nombre
+    
+    pre: recibe un string
+
+    post: devuelve None
+    """
+    with open("universidades.JSON", "r", encoding="utf-8") as entrada:
+        datos = json.load(entrada)
+        
+        filtrado = {clave: [] for clave in datos}
+
+        for indice, valor in enumerate(datos["Institución"]):
+            if nombre.lower() == valor.lower():
+                nombre_institucion = valor
+                for clave in datos:
+                    filtrado[clave].append(datos[clave][indice])
+
+    with open(f"{nombre_institucion}.JSON", "w", encoding="utf-8") as salida:
+        json.dump(filtrado, salida, ensure_ascii=False, indent=4)
+
+    return None
 
 def mujeres_egresadas():
-    pass
-
+    
 
 archivo_csv = "mujeres_programadoras_27032018.csv"
 
